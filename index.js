e4de73d97bef73ef93d102d42303143dba1d4113
Jose Albizures <jose@albizures.com>
2021-11-01 10:45:23 +0300

    feat(module): init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+	jsxSingleQuote: false,
+	singleQuote: true,
+	bracketSpacing: true,
+	bracketSameLine: true,
+	arrowParens: 'always',
+	endOfLine: 'lf',
+};
